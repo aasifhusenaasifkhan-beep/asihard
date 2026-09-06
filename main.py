c45e8beab1096430fd06553c8f443930fc94d713
aasifhusenaasifkhan-beep <aasifhusenaasifkhan@gmail.com>
2026-09-06 17:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -358,4 +358,4 @@ async def main():
     await app.stop()
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_until_complete(main()) 
